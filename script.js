fc9d3bcf2548e74660845566f6d1ffcfea89cf04
zopaj63 <zoran.pajnic@gmail.com>
2021-10-28 21:36:43 +0200

    connect and join room

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-const drone = new Scaledrone("VIgNNXjmYel6Vmhu");
+//connect to a channel
+const drone = new Scaledrone("VIgNNXjmYel6Vmhu", {
+    data: {
+        name: getName(),
+        color: getRandomColor(),
+    },
+});
+
+//connect to a room
+drone.on('open', error => {
+    if (error) {
+        return console.error(error);
+    }
+    console.log('Sucessfully conected to Scaledrone');
+
+    const room = drone.subscribe('observable-room');
+    room.on('open', error => {
+        if (error) {
+            return console.error(error);
+        }
+        console.log('Sucesfully joined room');
+    });
+
+});
+
+
+
+function getName() {
+    return ("Zoran");
+}
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
+
