9ece23bb0122dfe183eb0ec71cb36385c87c684d
zopaj63 <zoran.pajnic@gmail.com>
2021-10-29 18:49:43 +0200

    add html, css and handling users js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const drone = new Scaledrone("VIgNNXjmYel6Vmhu", {
     },
 });
 
+let members = [];
+
 //connect to a room
 drone.on('open', error => {
     if (error) {
@@ -21,7 +23,76 @@ drone.on('open', error => {
         console.log('Sucesfully joined room');
     });
 
+    room.on('members', m => {
+        members = m;
+        updateMembersDOM();
+    });
+
+    room.on('member_join', member => {
+        members.push(member);
+        updateMembersDOM();
+    });
+
+    room.on('member_leave', ({ id }) => {
+        const index = members.findIndex(member => member.id === id);
+        members.splice(index, 1);
+        updateMembersDOM();
+    });
+
+    room.on('data', (text, member) => {
+        if (member) {
+            addMessageToListDOM(text, member);
+        } else {
+            // Message is from server
+        }
+    });
 });
+
+//handling users (joining and leaving room)
+const DOM = {
+    membersCount: document.querySelector('.members-count'),
+    membersList: document.querySelector('.members-list'),
+    messages: document.querySelector('.messages'),
+    input: document.querySelector('.message-form__input'),
+    form: document.querySelector('.message-form'),
+};
+
+function createMemberElement(member) {
+    const { name, color } = member.clientData;
+    const el = document.createElement('div');
+    el.appendChild(document.createTextNode(name));
+    el.className = 'member';
+    el.style.color = color;
+    return el;
+}
+
+function updateMembersDOM() {
+    DOM.membersCount.innerText = `${members.length} users in room:`;
+    DOM.membersList.innerHTML = '';
+    members.forEach(member =>
+        DOM.membersList.appendChild(createMemberElement(member))
+    );
+}
+
+function createMessageElement(text, member) {
+    const el = document.createElement('div');
+    el.appendChild(createMemberElement(member));
+    el.appendChild(document.createTextNode(text));
+    el.className = 'message';
+    return el;
+}
+
+function addMessageToListDOM(text, member) {
+    const el = DOM.messages;
+    const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+    el.appendChild(createMessageElement(text, member));
+    if (wasTop) {
+        el.scrollTop = el.scrollHeight - el.clientHeight;
+    }
+}
+
+
+
 
 
 
