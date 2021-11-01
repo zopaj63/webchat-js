3a4b7f11c10b93f39d8fd4b15cf9cb90ec530d2b
zopaj63 <zoran.pajnic@gmail.com>
2021-11-01 20:32:31 +0100

    add getName function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //connect to a channel
 const drone = new Scaledrone("VIgNNXjmYel6Vmhu", {
     data: {
@@ -47,6 +48,34 @@ drone.on('open', error => {
         }
     });
 });
+
+drone.on('close', event => {
+    console.log('Connection was closed', event);
+});
+
+drone.on('error', error => {
+    console.error(error);
+});
+
+function getName() {
+    const member_name = "Member";
+    const memberNameForm = document.querySelector(".js-member-name");
+    console.log(memberNameForm);
+    const memberNameInput = memberNameForm.querySelector(".js-member-name__input");
+    console.log(memberNameInput);
+    memberNameForm.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("memberNameInput.value");
+        const member_name = memberNameInput.value;
+        console.log(member_name);
+    });
+    return member_name;
+}
+
+function getRandomColor() {
+    randomColor = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    return randomColor;
+}
 
 //handling users (joining and leaving room)
 const DOM = {
@@ -104,17 +133,3 @@ function addMessageToListDOM(text, member) {
         el.scrollTop = el.scrollHeight - el.clientHeight;
     }
 }
-
-
-
-
-
-
-function getName() {
-    return ("Zoran");
-}
-
-function getRandomColor() {
-    return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
-
